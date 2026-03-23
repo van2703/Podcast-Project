5736863485fbc658b095568fd6890b8c70d482b6
van2703 <vanphamx41@gmail.com>
2026-03-23 22:51:42 +0700

    Update podcast content and configuration; add new audio files and news articles, remove outdated entries, and enhance script for automated generation

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const PODCAST_LIST = ['audios/bbc_podcast_20260323_211837.wav', 'audios/bbc_podcast_20260323_172524.wav', 'audios/bbc_podcast_20260323_172012.wav', 'audios/bbc_podcast_20260323_170327.wav'];
-const LATEST_PODCAST = "audios/bbc_podcast_20260323_211837.wav";
+const PODCAST_LIST = ['audios/bbc_podcast_20260323_222734.wav', 'audios/bbc_podcast_20260323_211837.wav', 'audios/bbc_podcast_20260323_172524.wav', 'audios/bbc_podcast_20260323_172012.wav', 'audios/bbc_podcast_20260323_170327.wav'];
+const LATEST_PODCAST = "audios/bbc_podcast_20260323_222734.wav";
