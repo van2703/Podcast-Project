0cba900543521275f6c0772754362073f7bd3fa8
van2703 <vanphamx41@gmail.com>
2026-03-24 17:21:09 +0700

    Thêm podcast ngày hôm nay

--- config.js
+++ config.js
@@ -1,5 +1,83 @@
 const PODCAST_LIST = [
     {
+        "url": "audios/bbc_podcast_20260324_165532.mp3",
+        "title": "Global Currents: Vets, AI, and Geopolitical Shocks",
+        "summary": "From consumer protection and tech ethics to geopolitical tremors and aviation tragedies, we navigate the week's most compelling global narratives.",
+        "timestamp": "2026-03-24T16:55:40.843478"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_165228.mp3",
+        "title": "Global Currents: Vets, AI, and Geopolitical Shocks",
+        "summary": "From consumer protection and tech ethics to geopolitical tremors and aviation tragedies, we navigate the week's most compelling global narratives.",
+        "timestamp": "2026-03-24T16:52:38.344791"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_165037.mp3",
+        "title": "From Vet Relief to Global Turmoil: Unpacking Today's Headlines",
+        "summary": "We delve into vital consumer protections, startling corporate misconduct, significant tech shifts, and the ripple effects of global conflicts, alongside some truly bizarre international incidents.",
+        "timestamp": "2026-03-24T16:50:45.052327"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_164157.mp3",
+        "title": "From Vet Relief to Global Turmoil: Unpacking Today's Headlines",
+        "summary": "We delve into vital consumer protections, startling corporate misconduct, significant tech shifts, and the ripple effects of global conflicts, alongside some truly bizarre international incidents.",
+        "timestamp": "2026-03-24T16:42:09.827639"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_162842.mp3",
+        "title": "Global News: Diplomacy, Crashes, and Justice",
+        "summary": "Today, we cover international talks, plane crash investigations, protests, trade deals, fuel prices, and a major court ruling.",
+        "timestamp": "2026-03-24T16:29:21.137299"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_160438.mp3",
+        "title": "Global News: Diplomacy, Crashes, and Justice",
+        "summary": "Today, we cover international talks, plane crash investigations, protests, trade deals, fuel prices, and a major court ruling.",
+        "timestamp": "2026-03-24T16:05:13.381885"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_154422.mp3",
+        "title": "Global News: Diplomacy, Crashes, and Justice",
+        "summary": "Today, we cover international talks, plane crash investigations, protests, trade deals, fuel prices, and a major court ruling.",
+        "timestamp": "2026-03-24T15:44:50.740366"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_143841.wav",
+        "title": "Global Headlines: Diplomacy, Crashes, and Justice",
+        "summary": "This podcast covers global diplomacy, tragic plane crashes, economic moves, and a major legal ruling affecting a famous personality.",
+        "timestamp": "2026-03-24T14:38:46.360839"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_143303.wav",
+        "title": "Global Headlines: From Diplomacy to Tragic Accidents",
+        "summary": "Today we look at US President Trump's varied activities, tragic plane crashes, an alleged Russian plot in Angola, fuel price changes, trade deals, and a major legal verdict.",
+        "timestamp": "2026-03-24T14:33:12.478828"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_142416.wav",
+        "title": "Global Snapshot: Today's Key News Updates",
+        "summary": "We're looking at various important events happening globally, from politics to plane crashes and fuel prices.",
+        "timestamp": "2026-03-24T14:24:22.023494"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_141923.wav",
+        "title": "Global News: Deals, Tragedies, and Prices",
+        "summary": "Today's news covers global diplomacy, tragic plane crashes, an Angola protest trial, and China's fuel price adjustment.",
+        "timestamp": "2026-03-24T14:19:30.848953"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_141339.wav",
+        "title": "Global Events: From Diplomacy to Tragic Accidents",
+        "summary": "Today's podcast covers the US President's varied activities, a deadly plane crash at LaGuardia, a trial in Angola, a military plane crash in Colombia, and China's efforts to ease fuel costs.",
+        "timestamp": "2026-03-24T14:13:46.311465"
+    },
+    {
+        "url": "audios/bbc_podcast_20260324_140512.wav",
+        "title": "Global Headlines: Crashes, Diplomacy, and Fuel Costs",
+        "summary": "Today, we discuss plane crashes, US diplomacy with Iran, an alleged plot in Angola, and how China is handling fuel prices.",
+        "timestamp": "2026-03-24T14:05:15.813005"
+    },
+    {
         "url": "audios/bbc_podcast_20260324_134752.wav",
         "title": "Global News: Crashes, Politics, and Diplomacy",
         "summary": "Today, we cover tragic plane crashes, political trials and challenges, and the varied activities of the US president during wartime.",
@@ -67,4 +145,4 @@ const PODCAST_LIST = [
         "summary": "Today, we look at how US talks are affecting oil and shares, a strong warning about Earth's climate, and new police powers in Hong Kong. We also cover the passing of the OnlyFans owner and arrests in Nigeria."
     }
 ];
-const LATEST_PODCAST = "audios/bbc_podcast_20260324_134752.wav";
+const LATEST_PODCAST = "audios/bbc_podcast_20260324_165532.mp3";
