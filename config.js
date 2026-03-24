96db314d4262867b51849d377dff409ae487c33b
van2703 <vanphamx41@gmail.com>
2026-03-24 10:40:28 +0700

    Add new podcast episode and update pipeline for audio processing

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const PODCAST_LIST = [
     {
+        "url": "audios/bbc_podcast_20260324_103749.wav",
+        "title": "Global Events: Diplomacy, Tragedies, and Political Challenges",
+        "summary": "We cover the US President's diplomacy, two recent plane crashes, a trial in Angola, and political challenges in Italy. Join us for a simple look at the world's most important headlines."
+    },
+    {
         "url": "audios/bbc_podcast_20260323_232324.wav",
         "title": "Global Headlines: War, Crashes, Politics, and Fuel Limits",
         "summary": "We're looking at the human cost of war, a plane crash investigation, political upsets in Italy, and new fuel rules in Slovenia. Good news on talks to end war also brought market changes."
@@ -10,4 +15,4 @@ const PODCAST_LIST = [
         "summary": "Today, we look at how US talks are affecting oil and shares, a strong warning about Earth's climate, and new police powers in Hong Kong. We also cover the passing of the OnlyFans owner and arrests in Nigeria."
     }
 ];
-const LATEST_PODCAST = "audios/bbc_podcast_20260323_232324.wav";
+const LATEST_PODCAST = "audios/bbc_podcast_20260324_103749.wav";
